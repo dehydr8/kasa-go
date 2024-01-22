756f42e4d50c3bded698eee2efe4ac53e1992098
Osama Khalid <osama@codinghazard.com>
2024-01-22 12:16:40 -0500

    fix debug message

--- util/commit.go
+++ util/commit.go
@@ -1,13 +1,11 @@
 package util
 
 import (
-	"fmt"
 	"runtime/debug"
 )
 
 var Revision = func() string {
 	if info, ok := debug.ReadBuildInfo(); ok {
-		fmt.Println(info)
 		for _, setting := range info.Settings {
 			if setting.Key == "vcs.revision" {
 				return setting.Value[:7]
